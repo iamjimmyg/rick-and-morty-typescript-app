c823099038905ff887c1495cedfb9b4b5c616c8a
Jimmy Gonzalez <jimmygonzalez@Jimmys-MacBook-Pro.local>
2021-01-26 15:00:51 -0600

    first commit, added typescript

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <h1>Rick And Morty</h1>
+      <p>Pick your favourite episode!</p>
+    </React.Fragment>
+  )
+}
